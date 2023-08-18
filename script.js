4f535938b772ef5c32e1fecf444784eae6e677a1
jose <josematienzo101@gmail.com>
2023-08-18 11:23:31 +0800

    Compile basic operations

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, a, b) {
+    if (operator == 'add') {
+        return add(a, b);
+    } else if (operator == 'sub') {
+        return subtract(a, b);
+    } else if (operator == 'mul') {
+        return multiply(a, b);
+    } else if (operator == 'div') {
+        return divide(a, b);
+    }
+}
