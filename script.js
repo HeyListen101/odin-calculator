0bc1729662cb2265e8e887beca01edd704380803
jose <josematienzo101@gmail.com>
2023-08-23 11:58:03 +0800

    Fix decimal operation

--- script.js
+++ script.js
@@ -1,3 +1,75 @@
+const nums = document.querySelectorAll('.num');
+const operators = document.querySelectorAll('.operator');
+const decimal = document.querySelector('.decimal');
+const clear = document.querySelector('.clear');
+const back = document.querySelector('.back');
+const displayOne = document.querySelector('.display-one');
+const displayTwo = document.querySelector('.display-two');
+let currOperator = null;
+let valueA, valueB, decimalDiv, isDecimal;
+
+ document.body.addEventListener('keypress', event => {
+     displayOne.textContent += event.code.slice(5);
+ });
+
+nums.forEach(num => num.addEventListener('click', () => {
+    displayOne.textContent += num.textContent;
+    console.log(displayOne.textContent);
+    if (currOperator == null) {
+        displayTwo.textContent = displayOne.textContent;
+    } else if (!isDecimal && num.textContent != '.') {
+        valueB = valueB * 10 + Number(num.textContent);
+    } else if (num.textContent != '.') {
+        valueB = valueB + Number(num.textContent) / decimalDiv;
+        decimalDiv *= 10;
+    }
+
+    if (num.textContent == '.') isDecimal = true;
+
+    if (currOperator == '+') {
+        displayTwo.textContent = add(valueA, valueB).toString();
+    } else if (currOperator == '-') {
+        displayTwo.textContent = subtract(valueA, valueB).toString();
+    } else if (currOperator == 'x') {
+        displayTwo.textContent = multiply(valueA, valueB).toString();
+    } else if (currOperator == '/') {
+        displayTwo.textContent = divide(valueA, valueB).toString();
+    }
+}));
+
+operators.forEach(operator => operator.addEventListener('click', () => {
+    
+    valueA = Number(displayTwo.textContent);
+    valueB = 0;
+
+    if (!isOperator(displayOne.textContent[displayOne.textContent.length - 1]) && displayOne.textContent != '') {
+        displayOne.textContent += operator.textContent;
+        currOperator = operator.textContent;
+        isDecimal = false;
+        decimalDiv = 10;
+        if (currOperator == '=') displayOne.textContent = displayTwo.textContent;
+    }
+}));
+
+clear.addEventListener('click', () => {
+    displayOne.textContent = '';
+    displayTwo.textContent = '';
+    currOperator = null;
+    valueA = 0;
+    valueB = 0;
+    isDecimal = false;
+    decimalDiv = 10;
+});
+
+back.addEventListener('click', () => {
+    displayOne.textContent = displayOne.textContent.slice(0,displayOne.textContent.length - 1);
+    let lastChar = displayOne.textContent[displayOne.textContent.length - 1]; 
+    if (lastChar == '+' || lastChar == '-' || lastChar == '*' || lastChar == '/') {
+        displayTwo.textContent = Number(valueA);
+    }
+});
+
+
 function add(a, b) {
     return a + b;
 }
@@ -14,14 +86,6 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operator, a, b) {
-    if (operator == 'add') {
-        return add(a, b);
-    } else if (operator == 'sub') {
-        return subtract(a, b);
-    } else if (operator == 'mul') {
-        return multiply(a, b);
-    } else if (operator == 'div') {
-        return divide(a, b);
-    }
+function isOperator(o) {
+    return o == '+' || o == '-' || o == 'x' || o == '/' || o == '.';
 }
